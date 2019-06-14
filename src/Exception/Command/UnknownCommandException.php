3c5f891cba4363d18089d79b32379b27e1dbe048
Yannoff <yannoff.it@gmail.com>
2019-06-14 19:48:53 +0200

    Handle unknown command invocations
    - Provide `help` as default when invoked without command name
    - Identify & handle unknown command invocation cases

---
+++ src/Exception/Command/UnknownCommandException.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the yannoff/console library
+ * (c) 2019 Yannoff (https://github.com/yannoff)
+ *
+ * @project   yannoff/console
+ * @link      https://github.com/yannoff/console
+ * @license   http://opensource.org/licenses/MIT
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Yannoff\Component\Console\Exception\Command;
+
+use Throwable;
+use Yannoff\Component\Console\Exception\CommandException;
+
+/**
+ * Class UnknownCommandException
+ *
+ * Thrown when the application script is invoked with a command name unknown to the registry
+ *
+ * @package Yannoff\Component\Console\Exception\Command
+ */
+class UnknownCommandException extends CommandException
+{
+    /**
+     * UnknownCommandException constructor.
+     *
+     * @param string         $name     Name of the queried command
+     * @param int            $code     Error status code to be sent to the terminal (defaults to 127)
+     * @param Throwable|null $previous Optional parent exception
+     */
+    public function __construct($name = "", $code = 127, Throwable $previous = null)
+    {
+        $message = sprintf('Command with name "%s" not found by the application', $name);
+        parent::__construct($message, $code, $previous);
+    }
+}
